56e16e4e1fbeabb21bdf0cf2dc0bd5fa8acc0168
Poojitha Moparthi <poojidazzling@gmail.com>
2021-11-25 20:49:39 +0530

    Login Authentication modified

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express=require("express");
 const bodyParser = require("body-parser");
 
@@ -5,17 +6,46 @@ const app=express();
 const mongoose=require("mongoose");
 const fileRoutes=require("./routes/file");
 const path=require("path")
+const UserRoutes=require("./routes/user");
+const jwt=require("jsonwebtoken");
 
 mongoose.connect("mongodb://localhost/netflix");
 
 app.set('views', __dirname + '/views'); 
 app.set('view engine', 'ejs'); 
-app.use(bodyParser())
-app.get("/",function(req,res){
+app.use("/upload",function(req,res,next){
+    try{
+        const token = req.headers.authorization?.split(" ")[1];
+    console.log(token);
+    if (!token){
+        res.status(401).json({
+            status:"failed",
+            message:"Not Authenticated"
+        });
+        
+    }
+    const decoded=jwt.verify(token,"Secret-123");
+    if (!decoded){
+        return res.status(401).json({
+            status:"failed",
+            message:"Invalid token"
+        })
+    }
+    req.user=decoded.data //getting the user id from token
+    }catch(e){
+        return res.status(500).json({
+            status:"failed",
+            message:e.message
+        })
+    }
+    next();
+})
+app.use(bodyParser.json())
+app.get("/upload",function(req,res){
     res.render("./index")
 })
 
-
+app.use("/",UserRoutes);
 app.use("/upload-csv",fileRoutes);
 app.listen('3070', function(err){
     if(err){console.log("Error in running Server", err); return;}
